4a1fafcebdeedcd17506b685a26f9e01b3ea1b98
Miguel <mg1113565@gmail.com>
2026-09-08 09:33:04 -0700

    Add SymbolTable implementation

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -1,1 +1,41 @@
 #include "SymbolTable.h"
+
+SymbolTable::SymbolTable()
+{
+}
+
+void SymbolTable::addEntry(std::string symbol, int address)
+{
+    if (!(table.contains(symbol)))
+    {
+        table[symbol] = address;
+    }
+    // do nothing if entry already exists
+}
+
+bool SymbolTable::contains(std::string symbol)
+{
+
+    if (table.find(symbol) != table.end())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+// defo need to check contains before calling this
+int SymbolTable::getAddress(std::string symbol)
+{
+    int result{};
+
+    if (!(table.contains(symbol)))
+    {
+        result = table[symbol];
+    }
+
+    // returning 0 is the flag for not valid since we cant assign any labels or variable to 0
+    return result;
+}
--- SymbolTable.h
+++ SymbolTable.h
@@ -17,4 +17,4 @@ public:
 
 private:
     std::unordered_map<std::string, int> table;
-}
+};
