92d7b73b655d85b8aa8d0298fcb6813c8fcb848e
Miguel <mg1113565@gmail.com>
2026-09-08 10:03:27 -0700

    final working fixes

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -1,7 +1,25 @@
 #include "SymbolTable.h"
+#include <iostream>
 
 SymbolTable::SymbolTable()
 {
+    std::string base = "R";
+    for (int i = 0; i < 16; ++i)
+    {
+        base += std::to_string(i);
+        (*this).addEntry(base, i);
+        std::cout << base << " " << i << '\n';
+        base.erase(1);
+        i++;
+    }
+
+    (*this).addEntry("SCREEN", 16384);
+    (*this).addEntry("KBD", 24576);
+    (*this).addEntry("SP", 0);
+    (*this).addEntry("LCL", 1);
+    (*this).addEntry("ARG", 2);
+    (*this).addEntry("THIS", 3);
+    (*this).addEntry("THAT", 4);
 }
 
 bool SymbolTable::contains(std::string symbol)
@@ -31,7 +49,7 @@ int SymbolTable::getAddress(std::string symbol)
 {
     int result{};
 
-    if (!((*this).contains(symbol)))
+    if ((*this).contains(symbol))
     {
         result = table[symbol];
     }
